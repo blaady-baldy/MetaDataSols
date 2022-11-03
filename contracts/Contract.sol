8869bb8325d8644f3b212e047d47a1ddba072920
Devansh Singh <56143122+blaady-baldy@users.noreply.github.com>
2022-11-04 00:02:16 +0530

    Update Contract.sol

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,9 +2,21 @@
 pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC721Base.sol";
+import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 
-contract Contract is ERC721Base {
+contract Contract is ERC721Base, PermissionsEnumerable  {
 
+mapping(uint256 => uint256) public powerLevel;
+
+function setPowerLevel(uint256 _tokenId, uint256 _powerLevel) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    powerLevel[_tokenId] = _powerLevel;
+}
+
+function mintTo(address _to, string memory _tokenURI) public virtual override {
+    uint256 tokenId = nextTokenIdToMint();
+    super.mintTo(_to, _tokenURI);
+    powerLevel[tokenId] = tokenId;
+}
       constructor(
         string memory _name,
         string memory _symbol,
@@ -17,6 +29,8 @@ contract Contract is ERC721Base {
             _royaltyRecipient,
             _royaltyBps
         )
-    {}
+    {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
 }
